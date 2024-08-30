177433ecbc4557f1838e46ea74c29159adb58bcf
LocaZika <buinhupha@gmail.com>
2024-08-30 12:21:46 +0700

    Add files via upload

--- cars.sql
+++ cars.sql
@@ -1,13 +1,18 @@
 CREATE SCHEMA PRODUCT;
 
-CREATE TABLE PRODUCT.CARS(
+CREATE TYPE PRODUCTS.ENUM_CAR_TRANSMISSION AS ENUM('auto', 'manual', 'mixed');
+CREATE TYPE PRODUCTS.ENUM_CAR_TRADETYPE AS ENUM('sale', 'rent');
+CREATE TYPE PRODUCTS.ENUM_CAR_FUELTYPE AS ENUM('gasoline', 'electric', 'mixed');
+
+
+CREATE TABLE PRODUCTS.CARS(
 	ID SMALLINT GENERATED ALWAYS AS IDENTITY,
 	NAME VARCHAR(50) NOT NULL,
 	BRAND VARCHAR(15) NOT NULL,
 	PRICE NUMERIC(10,2) NOT NULL,
-	TRANSMISSION VARCHAR(6) NOT NULL,
-	TRADETYPE VARCHAR(4) NOT NULL,
-	FUELTYPE VARCHAR(10) NOT NULL,
+	TRANSMISSION PRODUCTS.ENUM_CAR_TRANSMISSION NOT NULL,
+	"tradeType" PRODUCTS.ENUM_CAR_TRADETYPE NOT NULL,
+	"fuelType" PRODUCTS.ENUM_CAR_FUELTYPE NOT NULL,
 	TYPE VARCHAR(10) NOT NULL,
 	HP SMALLINT NOT NULL,
 	MODEL SMALLINT NOT NULL,
@@ -16,27 +21,27 @@ CREATE TABLE PRODUCT.CARS(
 	CONSTRAINT PK_PRODUCT_CARS_ID PRIMARY KEY (ID, BRAND)
 ) PARTITION BY LIST (BRAND);
 
-CREATE TABLE AUDI PARTITION OF PRODUCT.CARS FOR VALUES IN ('audi');
-CREATE TABLE BMW PARTITION OF PRODUCT.CARS FOR VALUES IN ('bmw');
-CREATE TABLE FERRARI PARTITION OF PRODUCT.CARS FOR VALUES IN ('ferrari');
-CREATE TABLE FORD PARTITION OF PRODUCT.CARS FOR VALUES IN ('ford');
-CREATE TABLE HONDA PARTITION OF PRODUCT.CARS FOR VALUES IN ('honda');
-CREATE TABLE LAMBORGHINI PARTITION OF PRODUCT.CARS FOR VALUES IN ('lamborghini');
-CREATE TABLE MAZDA PARTITION OF PRODUCT.CARS FOR VALUES IN ('mazda');
-CREATE TABLE MERCEDES PARTITION OF PRODUCT.CARS FOR VALUES IN ('mercedes');
-CREATE TABLE OTHER_BRAND PARTITION OF PRODUCT.CARS DEFAULT;
+CREATE TABLE AUDI PARTITION OF PRODUCTs.CARS FOR VALUES IN ('audi');
+CREATE TABLE BMW PARTITION OF PRODUCTs.CARS FOR VALUES IN ('bmw');
+CREATE TABLE FERRARI PARTITION OF PRODUCTs.CARS FOR VALUES IN ('ferrari');
+CREATE TABLE FORD PARTITION OF PRODUCTs.CARS FOR VALUES IN ('ford');
+CREATE TABLE HONDA PARTITION OF PRODUCTs.CARS FOR VALUES IN ('honda');
+CREATE TABLE LAMBORGHINI PARTITION OF PRODUCTs.CARS FOR VALUES IN ('lamborghini');
+CREATE TABLE MAZDA PARTITION OF PRODUCTs.CARS FOR VALUES IN ('mazda');
+CREATE TABLE MERCEDES PARTITION OF PRODUCTs.CARS FOR VALUES IN ('mercedes');
+CREATE TABLE OTHER_BRAND PARTITION OF PRODUCTs.CARS DEFAULT;
 
-INSERT INTO PRODUCT.CARS(NAME, BRAND, PRICE, TRANSMISSION, TRADETYPE, FUELTYPE, TYPE, HP, MODEL, MILEAGE)
-SELECT NAME, BRAND, PRICE, TRANSMISSION, TRADETYPE, FUELTYPE, TYPE, HP, MODEL, MILEAGE
-FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
+INSERT INTO PRODUCTs.CARS(NAME, BRAND, PRICE, TRANSMISSION, "tradeType", "fuelType", TYPE, HP, MODEL, MILEAGE)
+SELECT NAME, BRAND, PRICE, TRANSMISSION, "tradeType", "fuelType", TYPE, HP, MODEL, MILEAGE
+FROM JSON_POPULATE_RECORDSET(NULL::PRODUCTS.CARS, '
 	[
     {
       "name": "audi a6",
       "brand": "audi",
       "price": 327,
       "transmission": "auto",
-      "tradetype": "rent",
-      "fueltype": "gasoline",
+      "tradeType": "rent",
+      "fuelType": "gasoline",
       "type": "sedan",
       "hp": 250,
       "model" : 2022,
@@ -47,8 +52,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "audi",
       "price": 471,
       "transmission": "auto",
-      "tradetype": "rent",
-      "fueltype": "gasoline",
+      "tradeType": "rent",
+      "fuelType": "gasoline",
       "type": "sportback",
       "hp": 250,
       "model" : 2023,
@@ -59,8 +64,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "bmw",
       "price": 71846.41,
       "transmission": "auto",
-      "tradetype": "sale",
-      "fueltype": "electric",
+      "tradeType": "sale",
+      "fuelType": "electric",
       "type": "sedan",
       "hp": 184,
       "model" : 2021,
@@ -71,8 +76,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "bmw",
       "price": 176671.51,
       "transmission": "auto",
-      "tradetype": "sale",
-      "fueltype": "gasoline",
+      "tradeType": "sale",
+      "fuelType": "gasoline",
       "type": "sedan",
       "hp": 286,
       "model" : 2023,
@@ -83,8 +88,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "ferrari",
       "price": 5000000,
       "transmission": "auto",
-      "tradetype": "sale",
-      "fueltype": "electric",
+      "tradeType": "sale",
+      "fuelType": "electric",
       "type": "sport",
       "hp": 700,
       "model" : 2016,
@@ -95,8 +100,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "ferrari",
       "price": 990000,
       "transmission": "auto",
-      "tradetype": "sale",
-      "fueltype": "electric",
+      "tradeType": "sale",
+      "fuelType": "electric",
       "type": "sport",
       "hp": 986,
       "model" : 2020,
@@ -107,8 +112,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "ford",
       "price": 62930,
       "transmission": "auto",
-      "tradetype": "sale",
-      "fueltype": "gasoline",
+      "tradeType": "sale",
+      "fuelType": "gasoline",
       "type": "suv",
       "hp": 500,
       "model" : 2024,
@@ -119,8 +124,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "ford",
       "price": 55620,
       "transmission": "auto",
-      "tradetype": "sale",
-      "fueltype": "gasoline",
+      "tradeType": "sale",
+      "fuelType": "gasoline",
       "type": "truck",
       "hp": 315,
       "model" : 2024,
@@ -131,8 +136,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "honda",
       "price": 35000,
       "transmission": "auto",
-      "tradetype": "sale",
-      "fueltype": "electric",
+      "tradeType": "sale",
+      "fuelType": "electric",
       "type": "suv",
       "hp": 190,
       "model" : 2024,
@@ -143,8 +148,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "honda",
       "price": 54280,
       "transmission": "manual",
-      "tradetype": "sale",
-      "fueltype": "electric",
+      "tradeType": "sale",
+      "fuelType": "electric",
       "type": "suv",
       "hp": 285,
       "model" : 2024,
@@ -155,8 +160,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "lamborghini",
       "price": 608.358,
       "transmission": "manual",
-      "tradetype": "sale",
-      "fueltype": "mixed",
+      "tradeType": "sale",
+      "fuelType": "mixed",
       "type": "sport",
       "hp": 813,
       "model" : 2024,
@@ -167,8 +172,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "lamborghini",
       "price": 3800000,
       "transmission": "auto",
-      "tradetype": "sale",
-      "fueltype": "electric",
+      "tradeType": "sale",
+      "fuelType": "electric",
       "type": "sport",
       "hp": 800,
       "model" : 2024,
@@ -179,8 +184,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "mazda",
       "price": 605,
       "transmission": "auto",
-      "tradetype": "rent",
-      "fueltype": "gasoline",
+      "tradeType": "rent",
+      "fuelType": "gasoline",
       "type": "sedan",
       "hp": 154,
       "model" : 2021,
@@ -191,8 +196,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "mazda",
       "price": 526,
       "transmission": "auto",
-      "tradetype": "rent",
-      "fueltype": "gasoline",
+      "tradeType": "rent",
+      "fuelType": "gasoline",
       "type": "suv",
       "hp": 188,
       "model" : 2021,
@@ -203,8 +208,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "mercedes",
       "price": 213,
       "transmission": "auto",
-      "tradetype": "rent",
-      "fueltype": "gasoline",
+      "tradeType": "rent",
+      "fuelType": "gasoline",
       "type": "sedan",
       "hp": 258,
       "model" : 2022,
@@ -215,8 +220,8 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
       "brand": "mercedes",
       "price": 447,
       "transmission": "auto",
-      "tradetype": "rent",
-      "fueltype": "gasoline",
+      "tradeType": "rent",
+      "fuelType": "gasoline",
       "type": "sedan",
       "hp": 367,
       "model" : 2024,
@@ -225,11 +230,11 @@ FROM JSON_POPULATE_RECORDSET(NULL::PRODUCT.CARS, '
   ]
 ');
 
-CREATE INDEX IDX_PRODUCT_CAR_ID ON PRODUCT.CARS(ID);
-CREATE INDEX IDX_PRODUCT_CAR_BRAND ON PRODUCT.CARS(BRAND);
-CREATE INDEX IDX_PRODUCT_CAR_NAME ON PRODUCT.CARS(NAME);
+CREATE INDEX IDX_PRODUCT_CAR_ID ON PRODUCTS.CARS(ID);
+CREATE INDEX IDX_PRODUCT_CAR_BRAND ON PRODUCTS.CARS(BRAND);
+CREATE INDEX IDX_PRODUCT_CAR_NAME ON PRODUCTS.CARS(NAME);
 
-EXPLAIN ANALYZE SELECT * FROM PRODUCT.CARS WHERE NAME ILIKE '%C-200%' AND BRAND = 'mercedes';
+EXPLAIN ANALYZE SELECT * FROM PRODUCTs.CARS WHERE NAME ILIKE '%C-200%' AND BRAND = 'mercedes';
 
 
 
